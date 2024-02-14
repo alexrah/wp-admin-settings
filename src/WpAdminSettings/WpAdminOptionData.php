9774158f82e69b19a8ade3274f104ed1ce6fe09a
alessandro stoppato <alexrah@gmail.com>
2024-02-14 14:12:14 +0100

    src/WpAdminSettings/WpAdminOptionData.php: add stub class to enforce types

---
+++ src/WpAdminSettings/WpAdminOptionData.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ale
+ * Date: 2020-04-16
+ * Time: 14:04
+ */
+
+namespace WpAdminSettings;
+
+/**
+ * @property string $name
+ * @property string $type
+ * @property string $label
+ * @property bool $required
+ * @property bool $admin_only
+ * @property array $params
+ *
+*/
+class WpAdminOptionData extends \stdClass { }
